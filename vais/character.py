4732f6e863a64aee311d358b5b77a6c3d0c2cebb
Duncan Murray <djmurray@acutesoftware.com.au>
2015-04-06 16:17:53 +0930

    fixing character.py and ref files to read in stats and generate random

--- vais/character.py
+++ vais/character.py
@@ -13,9 +13,11 @@ def TEST():
     """
     print('creating random character')
     traits = CharacterCollection(fldr)
-    print(traits)
-    #c = traits.generate_random_character('Zoltar')
-    #print(c)
+    #print(traits)
+    #print("traits.stats.raw_data:", traits.stats.dat)
+    #print("traits.stats:", str(traits.stats))
+    c = traits.generate_random_character('Zoltar')
+    print(c)
     
 class RefFile():
     """
@@ -26,22 +28,54 @@ class RefFile():
     """
     def __init__(self, fldr, fname):
         self.name = fname[0:-4]
-        self.raw_data = read_file(fldr + os.sep + fname)
-        self.dat = self.parse_to_dict(self.raw_data) # parse raw CSV into dictionary
-        
+        self.fname = fldr + os.sep + fname
+        self.hdrs = []
+        self.dat = self.parse_to_dict() # read CSV file and parse into dictionary
+
+    #def __iter__(self):
+    #    return iter(self.dat)
+            
     def __str__(self):
         res = ' === ' + self.name + ' Reference File ====\n'
-        res += str(len(self.raw_data)) + ' lines\n' 
+        res += str(len(self.dat)) + ' lines = ' 
+        for rownum, row in enumerate(self.dat):
+            if 'name' in self.hdrs:
+                res += row['name'] + ','
+            else:
+                #print("Headers for " + self.fname + " = " , self.hdrs)
+                pass
+            #for colnum, col in enumerate(row):
+            #    print(str(rownum), str(colnum), col)
         return res
     
-    def parse_to_dict(self, raw):
+    def parse_to_dict(self):
         """
         parse raw CSV into dictionary
         """
         lst = []
-        for i in raw:
-            lst.append({'name':i[0]})
+        with open(self.fname, 'r') as f:
+            hdr = f.readline()
+            self.hdrs = hdr.split(',')
+            #print("self.hdrs = ", self.hdrs)
+            for line in f:
+                cols = line.split(',')
+                if len(cols) == len(self.hdrs):
+                    #print(cols)
+                    dict = {}
+                    for ndx, col_header in enumerate(self.hdrs):
+                        #print("i[ndx] = ", cols[ndx].strip('\n').strip())
+                        dict[self.hdrs[ndx].strip('\n').strip()] = cols[ndx].strip('\n').strip()
+                    lst.append(dict)
+                else:
+                    print("Error parsing " + self.fname + " line : " + line)
         return lst
+    
+    def get_random_choice(self):    
+        """
+        returns a random name from the class
+        """
+        i = random.randint(0,len(self.dat)-1)
+        return self.dat[i]['name']
     
 class CharacterCollection():
     """
@@ -53,7 +87,7 @@ class CharacterCollection():
         to character traits
         """
         self.ref_folder = fldr
-        print('loading data files')
+        #print('loading data files')
         self.races = RefFile(fldr, 'ref_races.csv')
         self.classes = RefFile(fldr, 'ref_classes.csv')
         self.stats = RefFile(fldr, 'ref_stats.csv')
@@ -77,18 +111,21 @@ class CharacterCollection():
         uses the traits to create a random, but plausible
         character
         """
-        ch_class = random.choice(self.classes)
-        race = random.choice(self.races)
+        print(len(self.classes.dat))
+        print(self.classes.dat[0]['name'])
+        
+        ch_class = self.classes.get_random_choice()
+        race = self.races.get_random_choice()
         stats = self.random_stats(race, ch_class)
         skills = []
-        story = random.choice(self.stories)
+        story = self.stories.get_random_choice()
         inventory = ['5 gold']
         
         # pick random stuff here 
         for i in range(3):
-            inventory.append(self.inventory[random.randint(0,len(self.inventory)-1)])
+            inventory.append(self.inventory.get_random_choice())
         for i in range(3):
-            skills.append(self.skills[random.randint(0,len(self.skills)-1)])
+            skills.append(self.skills.get_random_choice())
         
         return Character(name, race, ch_class, stats, skills, story, inventory)
 
@@ -99,10 +136,10 @@ class CharacterCollection():
         base stats and applies a close random fit
         """
         stats = []
-        for ndx, i in enumerate(self.classes):
+        for ndx, i in enumerate(self.classes.dat):
             if i == race:
                 print(i)  # use stats for this class as baseline
-        for ndx, i in enumerate(self.races):
+        for ndx, i in enumerate(self.races.dat):
             if i == race:
                 print(i)  # use stats for this race to modify base stats
         
@@ -147,11 +184,13 @@ def read_file(fname):
     read a CSV file (ref_classes.csv) and return the 
     list of names
     """
+    print("NO - dont use this function read_file(fname):")
+    exit(1)
     lst = []
     with open(fname, 'r') as f:
-        hdr = f.readline()
+        #hdr = f.readline()
         for line in f:
-            lst.append(line)
+            lst.append(line.strip('\n'))
     return lst
     
 def read_file_1st_col_only(fname):
