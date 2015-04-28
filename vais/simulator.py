980b39f4b941fc2781911d8e269070fbe6a01a26
Duncan Murray <djmurray@acutesoftware.com.au>
2015-04-28 22:39:21 +0930

    adding verfiy agents and move agent

--- vais/simulator.py
+++ vais/simulator.py
@@ -14,7 +14,7 @@ def TEST():
     a3 = traits.generate_random_character()
     world = planet.Planet('SimWorld', num_seeds=5, width=20, height=15, wind=0.3, rain=0.10, sun=0.3, lava=0.4)
     actions = ['walk', 'run', 'fight', 'buy', 'sell', 'collect']
-    s = SimGameOfLife('Test of SimWorld', world, [a1, a2, a3], actions)
+    s = SimAdventureGame('Test of SimWorld', world, [a1, a2, a3], [(2,2), (3,4), (4,4)], actions)
     print(s)
     s.run()
     s.command({'name':'walk', 'type':'move', 'direction':[0,1]}, a1)
@@ -25,22 +25,44 @@ class Simulator():
     """
     Base simulator class
     """
-    def __init__(self, name, world, agents, actions):
+    def __init__(self, name, world, agents, agent_locations, actions):
         self.name = name
         self.world = world
-        self.agents = agents
         self.actions = actions
         self.events = []
         #self.log = aikif.log
         self.log = []
         self.status = 'Started'
+        self.agents = agents
+        self._verify_agents()
+        self.agent_locations = {}
+        for num, a in enumerate(self.agents):
+            print('a.name = ', a.name)
+            self.agent_locations[a.name] = agent_locations
+            print('self.agent_locations[a.name]',self.agent_locations[a.name])
      
     def __str__(self):
         res = ' -= ' + self.name + ' =--\n'
         res += 'world   = ' + self.world.name + ' (' + str(self.world.grid_width) + 'x' + str(self.world.grid_height) + ')\n'
-        res += 'agents  = ' + str(len(self.agents)) + ' (' + ','.join([c.name for c in self.agents]) + ')\n'
         res += 'actions = [' + ', '.join([a for a in self.actions]) + ']\n'
+        res += 'agents  = ' + str(len(self.agents)) + ' (' + ','.join([c.name for c in self.agents]) + ')\n'
+        res += 'agent_locations\n'
+        for a in self.agent_locations:
+            res += '\nagent ' + str(a[0]) + str(a[1])
         return res
+    
+    def _verify_agents(self):
+        """
+        verify agents passed to simulator by:
+        1. checking for unique names
+        2. TODO - other checks if needed
+        """
+        print('verifying...')
+        nmes = []
+        for a in self.agents:
+            nmes.append(a.name)
+        if len(nmes) != len(set(nmes)):
+            raise 'Error - you need to pass unique list of agent names to simulator'
     
     def run(self, num_iterations=-1):
         """
@@ -89,12 +111,22 @@ class Simulator():
         elif cmd['type'] == 'fight':
             print(agent.name, 'fights')
             
-        
+    def _move_agent(self, agent, direction, wrap_allowed=True):
+        """
+        moves agent 'agent' in 'direction'
+        """
     
+    
+class SimAdventureGame(Simulator):
+    def __init__(self, name, world, agents, agent_locations, actions):
+        Simulator.__init__(self, name, world, agents, agent_locations, actions)
+        
+    def __str__(self):
+        return 'Adventure Game ' + Simulator.__str__(self)
     
 class SimGameOfLife(Simulator):
-    def __init__(self, name, world, agents, actions):
-        Simulator.__init__(self, name, world, agents, actions)
+    def __init__(self, name, world, agents, agent_locations, actions):
+        Simulator.__init__(self, name, world, agents, agent_locations, actions)
         
     def __str__(self):
         return 'GAME OF LIFE ' + Simulator.__str__(self)
