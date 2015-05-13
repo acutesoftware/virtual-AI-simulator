56b9949f03947082907cbf20775a1abe9cf291f9
Duncan Murray <djmurray@acutesoftware.com.au>
2015-05-14 00:51:38 +0930

    build_internet creates random websites ready for testing

--- vais/build_internet.py
+++ vais/build_internet.py
@@ -6,26 +6,24 @@ import math
 from random import randint 
 import npc.web_users 
 
-#import aikif.environments.internet as my_world
+import aikif.environments.internet as mod_env
 
 #import aikif.agents.explore.agent_web_user as agt
-
-temp_folder = 'C:\\temp\\'
 
 fldr = os.getcwd() + os.sep + 'data'  + os.sep + 'worlds' 
 
 def main():
     """
     generates a virtual internet, sets pages and runs web_users on it
-    
-    myWorld = my_world.Internet( 5) 
-    myWorld.build_random( 6)
-    myWorld.grd.save(fldr + os.sep + 'vais_internet.txt')
     """
-
+    e = mod_env.Internet('VAIS - Load testing',  'Simulation of several websites')
+    e.create(50)
+    print(e)
     
     #Create some users to browse the web and load test website
-    print("TODO - wait for AIKIF 0.1.2 to be released")
     print(npc.web_users.params)
  
+    # Run the users over the sites at various reading speeds
+    # TODO 
+    
 main()
