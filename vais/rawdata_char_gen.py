44eba93f70d492ee8a0119477f20466498b8f860
Duncan Murray <djmurray@acutesoftware.com.au>
2015-06-28 00:52:51 +0930

    stub to test travis_ci

---
+++ vais/rawdata_char_gen.py
@@ -0,0 +1,20 @@
+# rawdata_char_gen.py
+
+import rawdata.generate
+
+
+def main():
+    """
+    testing building game characters via rawdata
+    """
+    print('testing building game characters via rawdata')
+    
+    
+    
+    print('Done')
+    
+    
+    
+if __name__ == '__main__':
+    main()    
+     
