e754ee801039918f2dba428983e0161ea055d18d
Duncan Murray <djmurray@acutesoftware.com.au>
2015-08-23 16:19:15 +0930

    adding worlds.py from AIKIF - may need to move it later to worldbuild

---
+++ tests/test_worlds.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# test_worlds.py
+import os
+import sys
+import unittest
+
+root_folder = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + ".." ) 
+pth = root_folder + os.sep + 'vais'
+
+sys.path.append(pth)
+
+import worlds
+
+class TestToolboxClsGridLife(unittest.TestCase):
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+
+        
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+
+    def test_01_world_init(self):
+        
+        num_seeds   =   6   # number of seed points to start land generation
+        perc_land   =  20   # % of world that is land
+        perc_sea    =  80   # % of world that is sea
+        perc_blocked=   4   # % of world that is blocked
+        
+        myWorld = worlds.World( 22, 44, ['O','P','@'])  # TODO - fix passing
+        self.assertEqual(str(myWorld.grd)[0:44], '............................................')
+        self.assertEqual('.' in str(myWorld.grd), True)
+        self.assertEqual('X' in str(myWorld.grd), False)  # should be all blank before we build 
+        self.assertEqual('#' in str(myWorld.grd), False)  # the world using build random
+        
+        myWorld.build_random( num_seeds, perc_land, perc_sea, perc_blocked)
+        print(myWorld.grd)
+        self.assertEqual(myWorld.grd.get_grid_width(), 44)
+        self.assertEqual(myWorld.grd.get_grid_height(), 22)
+        
+        self.assertEqual('.' in str(myWorld.grd), True)
+        self.assertEqual('X' in str(myWorld.grd), True)
+        self.assertEqual('#' in str(myWorld.grd), True)
+               
+if __name__ == '__main__':
+    unittest.main()
