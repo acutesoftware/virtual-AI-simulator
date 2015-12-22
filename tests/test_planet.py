1f92240f47b8da5273bf4e412e7b533ead7bc6ef
Duncan Murray <djmurray@acutesoftware.com.au>
2015-12-23 00:09:40 +1030

    tests for planet str()

--- tests/test_planet.py
+++ tests/test_planet.py
@@ -16,20 +16,17 @@ sys.path.append(root_folder)
 
 import planet 
 
-class VaisCharacterTest(unittest.TestCase):
-    def setup(self):
-        print('running planet tests')
-        
+class VaisPlanetTest(unittest.TestCase):
     def test_01_create_planet(self):
-        p = planet.Planet('Divitie', num_seeds=5, width=20, height=15, wind=0.1, rain=0.50, sun=0.4, lava=0.2)
-        p.evolve(years=10000000)
+        p = planet.Planet('Planet001', num_seeds=5, width=20, height=15, wind=0.1, rain=0.50, sun=0.4, lava=0.2)
+        p.evolve(years=1000000)
         print(p)
+        self.assertEqual(str(p)[0:24], '\n-- Welcome to Planet001')
 
     def test_02_large_planet(self):
-        p2 = planet.Planet('Jupiter', num_seeds=8, width=200, height=150, wind=0.5, rain=0.90, sun=0.6, lava=0.5)
-        p2.evolve(years=10000000)
-        #p2.save()
-        print(p2)
+        p2 = planet.Planet('Planet002', num_seeds=8, width=20, height=150, wind=0.5, rain=0.90, sun=0.6, lava=0.5)
+        p2.evolve(years=1000000)
+        self.assertEqual(str(p2)[0:24], '\n-- Welcome to Planet002')
 
     
 if __name__ == '__main__':
