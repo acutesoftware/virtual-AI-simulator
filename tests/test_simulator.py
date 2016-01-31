eb3939a1c488a75c6524317da624111fef621f30
Duncan Murray <djmurray@acutesoftware.com.au>
2016-01-31 20:54:12 +1030

    test using world and agents instead of planets compiles and runs but no actual tests

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -14,6 +14,7 @@ import planet as planet
 import battle as battle
 import character as character
 import simulator as simulator
+import worlds
 
 test_folder = os.getcwd() + os.sep + 'test_results'
 test_file = test_folder + os.sep + 'battle.txt'
@@ -52,20 +53,66 @@ class VaisSimulatorTest(unittest.TestCase):
         self.assertEqual(s.agent_locations[0]['y'], 2)  
         
         s.command({'name':'walk', 'type':'move', 'direction':[1,1]}, a1)
+        s.command({'name':'run', 'type':'run', 'direction':[2,1]}, a1)
+        s.command({'name':'fight', 'type':'fight', 'direction':[2,1]}, a1)
+       
         
-    def test_03_SimGameOfLife(self):
+    def test_03_sim_fail(self):
+        self.assertNotEqual(1, 2)  
+    
+    def test_05_run_aikif_agent(self):
+        """
+        recreates agent list as per world_generator and puts them in a world.
+        NOTE - uses World object, not planet
+        """
+        import math
+        from random import randint
+        import aikif.agents.explore.agent_explore_grid as agt
+
+        log_folder = os.path.join( os.getcwd(), 'test_results')
+        
+        myWorld = worlds.World( 40, 40, ['.','X','#'])
+        myWorld.build_random( 5, 60, 30, 10)
+        target_coords = [math.floor(myWorld.grd.grid_height/2) + randint(1, math.floor(myWorld.grd.grid_height/2)) - 3, \
+             math.floor(myWorld.grd.grid_width /2) + randint(1, math.floor(myWorld.grd.grid_width/2)) - 5]
+        agt_list = []
+        for agt_num in range(0,4):
+
+            ag = agt.ExploreAgent( 'exploring_agent' + str(agt_num),  log_folder, False,1)
+            start_y, start_x = myWorld.grd.find_safe_starting_point()
+            
+            ag.set_world(myWorld.grd, [start_y, start_x], [target_coords[0], target_coords[1]])
+            agt_list.append(ag)
+        
+    
+        sim = worlds.WorldSimulation(myWorld, agt_list, 1)
+        sim.run(9, 'Y', log_folder + os.sep)
+    
+    
+    
+    def test_11_SimGameOfLife(self):
         traits = character.CharacterCollection(ref_folder)
         a1 = traits.generate_random_character()
         world = planet.Planet('SimWorld', num_seeds=5, width=20, height=15, wind=0.3, rain=0.10, sun=0.3, lava=0.4)
         actions = ['walk']
-        s = simulator.SimAdventureGame('Test of SimWorld', world, [a1], [(2,2)], actions)
+        s = simulator.SimAdventureGame('Test of Game of Life', world, [a1], [(2,2)], actions)
         s.run()
         print(s)
-        self.assertEqual(len(str(s)), 143)  
-    
-    def test_04_sim_fail(self):
-        self.assertNotEqual(1, 2)  
+        self.assertEqual(len(str(s)), 147)  
         
+        
+    
+    def test_21_SimAdventureGame(self):
+        traits = character.CharacterCollection(ref_folder)
+        a1 = traits.generate_random_character()
+        world = planet.Planet('SimWorld', num_seeds=5, width=20, height=15, wind=0.3, rain=0.10, sun=0.3, lava=0.4)
+        actions = ['walk']
+        s = simulator.SimAdventureGame('Test of SimAdventureGame', world, [a1], [(2,2)], actions)
+        s.run()
+        print(s)
+        self.assertEqual(len(str(s)), 151)  
+        
+    
 if __name__ == '__main__':
     unittest.main() 
          
