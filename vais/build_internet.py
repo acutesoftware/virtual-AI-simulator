5aa8a3e452076ec6402d312f19eb7d6470ce04fd
Duncan Murray <djmurray@acutesoftware.com.au>
2016-04-17 00:07:28 +0930

    testing internet user simulation

--- vais/build_internet.py
+++ vais/build_internet.py
@@ -7,14 +7,14 @@ import aikif.environments.internet as mod_env
 
 #import aikif.agents.explore.agent_web_user as agt
 
-fldr = os.getcwd() + os.sep + 'data'  + os.sep + 'worlds' 
+fldr = os.path.join(os.getcwd(),'data','worlds')
 
 def main():
     """
     generates a virtual internet, sets pages and runs web_users on it
     """
     e = mod_env.Internet('VAIS - Load testing',  'Simulation of several websites')
-    e.create(50)
+    e.create(800)
     print(e)
     
     #Create some users to browse the web and load test website
