64208afa60a2ab9d3de952a90a07f9f320926017
Duncan Murray <djmurray@acutesoftware.com.au>
2016-05-25 22:27:58 +0930

    calculate wall and roof claddings

--- vais/z_prototypes/calc_building_design.py
+++ vais/z_prototypes/calc_building_design.py
@@ -7,13 +7,29 @@
 # be called with different parameters to test different
 # scenarios.
 
+from pprint import pprint
+
 def main():
     print('calc_building_design')
     w = int(input('enter building width : '))
     l = int(input('enter building length : '))
     
-    print('area =  ', w * l)
-    print('perim = ', 2 * w + 2 * l)
 
+    res = calc_structure(width=w, length=l, height=2.4, prevailing_wind=3.2)
+    pprint(res)
+    
+def calc_structure(width, length, height, prevailing_wind=2.8 ):   
+    """
+    calculate various aspects of the structure 
+    (WARNING - these are not complete engineering specs, 
+     rather guideline as exercise for this software)
+    """
+    area = width * length
+    perim =  2 * width + 2 * length
+    roof_cladding = area
+    wall_cladding = perim * height
+    
+    return ({'area':area, 'perim':perim, 'roof_cladding':roof_cladding, 'wall_cladding':wall_cladding})
+    
 if __name__ == '__main__':
     main()
