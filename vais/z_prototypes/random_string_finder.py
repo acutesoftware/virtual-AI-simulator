8eb1d3113e8868a577150fae93e8d124f5fca9df
Duncan Murray <djmurray@acutesoftware.com.au>
2016-06-03 22:01:21 +0930

    playing with random number generator to get words

---
+++ vais/z_prototypes/random_string_finder.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# random_string_finder.py
+
+import sys
+import random
+
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+handler = logging.FileHandler(__name__ + '.log')
+handler.setLevel(logging.INFO)
+
+words_to_find = ['MATHS', 'HELP', 'maths', 'ROCKS', 'Maths', 'Rules', 'LOL', 'WTF']
+
+
+def main():
+    #method1()
+    #method2()
+    get_found_locations()
+    
+
+def method1():
+    for start_seed in range(1, 9999):
+        random.seed(start_seed)
+        if start_seed % 100 == 0:
+            logger.debug('seed = ' + str(start_seed))
+            sys.stdout.write('.') 
+            sys.stdout.flush()
+        garbage = ''.join([chr(random.randint(32,122)) for x in range(100000)])
+        for word in words_to_find:
+            if word in garbage:
+                logger.info('found ' + word + ' in ' + str(start_seed))
+        
+  
+def get_found_locations():
+    """
+    INFO:__main__:found HELP in 1572
+    INFO:__main__:found MATHS in 1704
+    INFO:__main__:found ROCKS in 1975
+    #random.seed(1572)
+    #garbage = ''.join([chr(random.randint(32,122)) for x in range(100000)])
+    #wrd_location = garbage.find('HELP')
+    #print(wrd_location) # 73834
+    #print(garbage[wrd_location:wrd_location+4])  # HELP
+    """
+    #random.seed(1704)
+    #garbage = ''.join([chr(random.randint(32,122)) for x in range(100000)])
+    #wrd_location = garbage.find('MATHS')
+    #print(wrd_location) # 73834
+    #print(garbage[wrd_location:wrd_location+5])  # HELP
+    
+    random.seed(1704)
+    print(''.join([chr(random.randint(32,122)) for x in range(100000)])[59741:59746])
+    random.seed(1572)
+    print(''.join([chr(random.randint(32,122)) for x in range(100000)])[73834:73838])
+    
+    
+    
+    
+    
+main()                
